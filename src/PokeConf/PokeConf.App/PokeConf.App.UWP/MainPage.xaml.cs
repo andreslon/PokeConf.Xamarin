049c2f12d83e23d5df2f28fd4ad0e137c5fda934
Andrés Londoño <andreslon@outlook.com>
2017-11-06 11:02:12 -0500

    Initial project

---
+++ src/PokeConf/PokeConf.App/PokeConf.App.UWP/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PokeConf.App.UWP
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            LoadApplication(new PokeConf.App.App());
+        }
+    }
+}
