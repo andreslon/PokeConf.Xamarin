50ef8a6ca72c0561a7463414f16d4f74042693b2
Andreslon <andres_londono@satrack.com>
2017-11-07 09:09:11 -0500

    Implemented UITest

--- README.md
+++ README.md
@@ -8,3 +8,24 @@ DevOps es una metodologia para la creación de software que se basa en un cambio
 Para este laboratorio se ha construido un aplicativo movil que requiere de nuevas funcionalidades, por lo que los desarrolladores deberan establecer un esquema de entrega continua agil mediante herramientas de DevOps, en nuestro caso, se utilizara la plataforma de mobile center, encargada de gestionar, probar y distribuir nuestro aplicativo movil realizado en Xamarin.
 
 # 
+
+
+## Pruebas UI
+
+En el proceso de DevOps uno de los pasos más importantes que asegura la calidad del producto y seguridad a cliente, son las pruebas de la aplicación en entornos reales, como sabemos existen miles variaciones de dispositivos, según marca, versión sistema operativo y caracteristicas.
+
+A continuación realizaremos las configuraciones necesarias y ejecutaremos el codugo de pruebas de interfaz escritos con anterioridad.
+
+1. Instalar Node.js, version 6.3 o superior
+1. Abrir una terminal.
+1. Instalar el paquete npm mobile-center-cli con el comando: 
+```
+npm install -g mobile-center-cli
+```
+1. Es necesario loguearnos en la terminal con el comando: 
+```
+mobile-center login
+```
+1. 
+
+![recursos](images/1.png)
--- src/PokeConf/PokeConf.App/PokeConf.App.UITests/Tests.cs
+++ src/PokeConf/PokeConf.App/PokeConf.App.UITests/Tests.cs
@@ -32,17 +32,31 @@ namespace PokeConf.App.UITests
 
         } 
         [Test]
-        public void PokedexList()
-        {
-            app.WaitForElement(x => x.Text("pikachu"));
+        public void PokedexFilter()
+        { 
+            app.WaitFor(() => app.Query(e => e.Class("ListView")).Count()>0);
+            app.Screenshot("Lista de pokemones cargada");
             app.DoubleTap(x => x.Id("search_src_text"));
-            app.Screenshot("Double tapped on view with class: SearchView$SearchAutoComplete with id: search_src_text");
+            app.Screenshot("Barra de busqueda seleccionada");
             app.EnterText(x => x.Id("search_src_text"), "pika");
             app.DismissKeyboard();
+            app.Screenshot("Filtrar por pikachu en la lista");
             app.Tap(x => x.Text("pikachu"));
-            app.Screenshot("Tapped on view with class: FormsTextView with text: pikachu");
+            app.Screenshot("Navegar a detalle de pikachu");
             app.Back();
-            app.Screenshot("Tapped on view with class: AppCompatImageButtonBack");
+            app.Screenshot("Regresar a la lista de pokemones");
         } 
+        [Test]
+        public void PokedexList()
+        { 
+            app.WaitFor(() => app.Query(e => e.Class("ListView")).Count() > 0);
+            app.Screenshot("Lista de pokemones cargada"); 
+            app.ScrollDown();
+            app.ScrollDown(); 
+            app.Screenshot("Scroll Down realizado"); 
+            app.ScrollUp();
+            app.ScrollUp();
+            app.Screenshot("Scroll Up realizado");
+        }
     }
 }
